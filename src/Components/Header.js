8d8e8d2103d9a4fdf972887114b55efc2bb2ff9e
Kelvin Mbewe <keokeil@hotmail.com>
2017-06-28 00:50:18 +0200

    header text from json :grinning::grinning:

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,9 @@ class Header extends Component {
   render() {
     if(this.props.data){
         var name = this.props.data.name;
+        var occupation = this.props.data.occupation;
+        var description = this.props.data.description;
+        var city = this.props.data.address.city;
     }
     return (
       <header id="home">
@@ -28,9 +31,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name} .</h1>
-            <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span> creating awesome and
-            effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling</a>
-            and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
+            <h3>I'm a {city} based <span>{occupation} </span> {description}</h3>
             <hr />
             <ul className="social">
                <li><a href=""><i className="fa fa-facebook"></i></a></li>
