bf2e655fb206f4587d88d11802c57a783718108e
Kelvin Mbewe <keokeil@hotmail.com>
2017-06-28 06:30:10 +0200

    footer.js update :laughing::laughing:

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -28,30 +28,30 @@ class Contact extends Component {
         <div className="row">
            <div className="eight columns">
               <form action="" method="post" id="contactForm" name="contactForm">
-         <fieldset>
-                 <div>
-              <label htmlFor="contactName">Name <span className="required">*</span></label>
-              <input type="text" value="" size="35" id="contactName" name="contactName" />
-                 </div>
-                 <div>
-              <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-              <input type="text" value="" size="35" id="contactEmail" name="contactEmail" />
-                 </div>
-                 <div>
-              <label htmlFor="contactSubject">Subject</label>
-              <input type="text" value="" size="35" id="contactSubject" name="contactSubject" />
-                 </div>
-                 <div>
-                    <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                    <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                 </div>
-                 <div>
-                    <button className="submit">Submit</button>
-                    <span id="image-loader">
-                       <img alt="" src="images/loader.gif" />
-                    </span>
-                 </div>
-         </fieldset>
+                   <fieldset>
+                           <div>
+                        <label htmlFor="contactName">Name <span className="required">*</span></label>
+                        <input type="text" value="" size="35" id="contactName" name="contactName" />
+                           </div>
+                           <div>
+                        <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+                        <input type="text" value="" size="35" id="contactEmail" name="contactEmail" />
+                           </div>
+                           <div>
+                        <label htmlFor="contactSubject">Subject</label>
+                        <input type="text" value="" size="35" id="contactSubject" name="contactSubject" />
+                           </div>
+                           <div>
+                              <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                              <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                           </div>
+                           <div>
+                              <button className="submit">Submit</button>
+                              <span id="image-loader">
+                                 <img alt="" src="images/loader.gif" />
+                              </span>
+                           </div>
+                   </fieldset>
           </form>
               <div id="message-warning"> Error boy</div>
           <div id="message-success">
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,11 +5,8 @@ class Footer extends Component {
   render() {
     return (
         <footer>
-
       <div className="row">
-
          <div className="twelve columns">
-
             <ul className="social-links">
                <li><a href=""><i className="fa fa-facebook"></i></a></li>
                <li><a href=""><i className="fa fa-twitter"></i></a></li>
@@ -19,18 +16,12 @@ class Footer extends Component {
                <li><a href=""><i className="fa fa-dribbble"></i></a></li>
                <li><a href=""><i className="fa fa-skype"></i></a></li>
             </ul>
-
             <ul className="copyright">
-               <li>&copy; Copyright 2014 CeeVee</li>
-               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+               <li>&copy; Copyright 2017 CeeVee</li>
             </ul>
-
          </div>
-
          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-
       </div>
-
    </footer>
     );
   }
